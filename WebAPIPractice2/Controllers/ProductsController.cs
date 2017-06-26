347f5c30f8a486b96c17d55018ae3ce2588330eb
weizhe0422 <weizhe.chang@gmail.com>
2017-06-27 00:31:02 +0800

    no message

---
+++ WebAPIPractice2/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIPractice2.Models;
+namespace WebAPIPractice2.Controllers
+{
+    /// <summary>
+    /// use to do Product class controller
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[] 
+        { 
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M } 
+        };
+
+        /// <summary>
+        /// Get all product's data
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks> get products' data</remarks>
+        /// <response code="404">Not found</response>
+        /// <response code="500">Internal Server Error</response>
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ WebAPIPractice2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPIPractice2.WebApiApplication" Language="C#" %>
